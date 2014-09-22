0d2ceefd15d8a1f7818f617a86b1d23009fdd3fd
Gabor Csardi <csardi.gabor@gmail.com>
2014-09-22 16:41:09 -0400

    Add has_color() function

--- R/crayon-package.r
+++ R/crayon-package.r
@@ -4,3 +4,31 @@
 #' @docType package
 #' @name crayon-package
 NULL
+
+
+## ----------------------------------------------------------------------
+
+#' Does the current R session support color?
+#'
+#' @return \code{TRUE} is the current R session supports color.
+#'
+#' @export
+
+has_color <- function() {
+
+  ## Are we in a terminal? No?
+  if (!isatty(stdout())) { return(FALSE) }
+
+  ## Are we in a windows terminal?
+  ## TODO
+
+  ## COLORTERM set?
+  if ("COLORTERM" %in% names(Sys.getenv())) { return(TRUE) }
+
+  ## dumb terminal is not good
+  if (Sys.getenv("TERM") == "dumb") { return(FALSE) }
+
+  ## Otherwise try to guess based on TERM
+  grepl("^screen|^xterm|^vt100|color|ansi|cygwin|linux",
+        Sys.getenv("TERM"), ignore.case = TRUE, perl = TRUE)
+}
