4337fa663c91ed8b90a118384c012b9f2454d40e
Gabor Csardi <csardi.gabor@gmail.com>
2014-09-23 12:01:39 -0400

    Vectorise %+%

--- R/string.r
+++ R/string.r
@@ -9,17 +9,46 @@
 
 chr <- function(x, ...) as.character(x, ...)
 
-#' Concatenate strings
+#' Concatenate character vectors
 #'
-#' @param lhs Left hand side, character scalar.
-#' @param rhs Right hand side, character scalar.
-#' @return Concatenated string.
+#' The length of the two arguments must match, or
+#' one of them must be of length one. If the length of
+#' one argument is one, then the output's length will
+#' match the length of the other argument. See examples
+#' below.
+#'
+#' @param lhs Left hand side, character vector.
+#' @param rhs Right hand side, character vector.
+#' @return Concatenated vectors.
 #'
 #' @name concat
 #' @export
+#'
+#' @examples
+#' "foo" %+% "bar"
+#'
+#' letters[1:10] %+% chr(1:10)
+#'
+#' letters[1:10] %+% "-" %+% chr(1:10)
+#'
+#' ## This is empty (unlike for parse)
+#' character() %+% "*"
 
 `%+%` <- function(lhs, rhs) {
-  check_string(lhs)
-  check_string(rhs)
-  paste0(lhs, rhs)
+
+  stopifnot(is.character(lhs), is.character(rhs))
+  stopifnot(length(lhs) == length(rhs) || length(lhs) == 1 || length(rhs) == 1)
+
+  if (length(lhs) == 0 && length(rhs) == 0) {
+    paste0(lhs, rhs)
+
+  } else if (length(lhs) == 0) {
+    lhs
+
+  } else if (length(rhs) == 0) {
+    rhs
+
+  } else {
+    paste0(lhs, rhs)
+  }
 }
