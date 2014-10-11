005af5a73d2f8fce426f4019417353a177dea744
Gabor Csardi <csardi.gabor@gmail.com>
2014-10-11 08:42:19 -0400

    Fix mixing bg and fg of ANSI 256, towards #1

--- R/ansi-256.r
+++ R/ansi-256.r
@@ -1,9 +1,10 @@
 
-fgcodes <- paste0('\x1b[;38;5;', 0:255, 'm')
-bgcodes <- paste0('\x1b[;48;5;', 0:255, 'm')
+fgcodes <- c(paste0('\x1b[38;5;', 0:255, 'm'), '\x1b[39m')
+bgcodes <- c(paste0('\x1b[48;5;', 0:255, 'm'), '\x1b[49m')
 
 rgb_index <- 17:232
 gray_index <- 233:256
+reset_index <- 257
 
 ansi256 <- function(rgb, bg = FALSE) {
   codes <- if (bg) bgcodes else fgcodes
@@ -11,7 +12,7 @@ ansi256 <- function(rgb, bg = FALSE) {
     ## Gray
     list(
       open = codes[gray_index][scale(rgb[1], to = c(0, 23))],
-      close = builtin_styles$reset$close
+      close = codes[reset_index]
     )
     
   } else {
@@ -19,7 +20,7 @@ ansi256 <- function(rgb, bg = FALSE) {
     rgb <- scale(rgb)
     list(
       open = codes[rgb_index][rgb[1] * 36 + rgb[2] * 6 + rgb[3] + 1],
-      close = builtin_styles$reset$close
+      close = codes[reset_index]
     )
   }
 }
