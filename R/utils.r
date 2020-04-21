9c0749c00324f26b7421448b2eefe310162fed70
Ista Zahn <izahn@hbs.edu>
2020-04-21 09:48:05 -0400

    Avoid warning if INSIDE_EMACS does not contain version.
    The INSIDE_EMACS environment variable is not guaranteed to include the version
    number. For example, https://github.com/akermu/emacs-libvterm sets it to
    "vterm". To avoid warnings in this case we remove all non-numeric characters and
    return NA if nothing remains.

--- R/utils.r
+++ R/utils.r
@@ -95,10 +95,8 @@ myseq <- function(from, to, by = 1) {
 
 emacs_version <- function() {
   ver <- Sys.getenv("INSIDE_EMACS")
+  ver <- gsub("[^0-9\\.]+", "", ver)  
   if (ver == "") return(NA_integer_)
-
-  ver <- gsub("'", "", ver)
-  ver <- strsplit(ver, ",", fixed = TRUE)[[1]]
   ver <- strsplit(ver, ".", fixed = TRUE)[[1]]
   as.numeric(ver)
 }
